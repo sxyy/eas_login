ab1b2274b89e295b7f535d591eca245633f28b9c
zhengshengxiong <zhengshengxiong@cvte.com>
2021-04-14 23:37:48 +0800

    :init: 初始化

---
+++ README.md
@@ -0,0 +1,8 @@
+### 操作步骤
+
+1. git clone 仓库 
+2. 下载 [node](https://nodejs.org/dist/latest-v12.x/node-v12.22.1-x64.msi)应用
+3. 进入到对应的目录下 运行 npm install --registry https://registry.npm.taobao.org
+4. 修改index.js 中的  用户名以及密码。
+5. 将excel表重新保存为csv文件，名字叫做名单保存到项目目录下
+6. 直接运行node index.js 即可
---
+++ index.js
@@ -0,0 +1,90 @@
+const JSEncrypt = require('node-jsencrypt');
+const request = require('request');
+const async = require('async');
+const encrypt = new JSEncrypt();
+const cheerio = require('cheerio');
+const csv = require('csv-parser');
+var fs = require('fs');
+
+var publicKey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAN79UcUg0f55hfR2VzpvnYgWVHOgRJd1PEVKE2u9sMsbPWhrndKooXpvL3Fs8JUBRUtn7TLh6Cfr86IM17L7bBsCAwEAAQ==";
+encrypt.setPublicKey(publicKey);
+var username = encrypt.encrypt("用户名");
+var password = encrypt.encrypt("密码")
+var verifycode = encrypt.encrypt("")
+var myJar = request.jar();
+const HOST_URL = 'https://eas.xhd.cn'
+const results = [];
+
+
+async.waterfall([
+    done => {
+        request({
+            url: HOST_URL + '/platform/login/vali',
+            method: 'POST',
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
+            },
+            form: {
+                "username": username,
+                "password": password,
+                "verifycode": verifycode,
+            },
+            json: true,
+            jar: myJar
+        }, (error, resp, data) => {
+            if (data.status === 'success') {
+                console.log("登录成功")
+                done(null)
+            } else {
+                console.log(data.status);
+                console.log(data);
+                done("登录失败");
+            }
+        })
+    },
+    done => {
+        fs.createReadStream('名单.csv')
+            .pipe(csv({
+                headers: false
+            }))
+            .on('data', (row) => {
+                results.push(row);
+            })
+            .on('end', () => {
+                async.mapSeries(results, (item, done1) => {
+                    request({
+                        url: HOST_URL + '/sales/clue/allQueries',
+                        method: 'POST',
+                        headers: {
+                            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
+                        },
+                        form: {
+                            "_query.customername": "",
+                            "_query.mobile": item['1'],
+                            "_query.qq": "",
+                            "_query.wechat": "",
+                            "pageNumber": 1,
+                            "pageNumber": 1,
+                            "pageSize": "",
+                            "orderColunm": "",
+                            "orderMode": ""
+                        },
+                        jar: myJar
+                    }, (error, resp, data) => {
+                        $ = cheerio.load(data);
+                        if ($('#leadsTable tbody tr td').length > 0) {
+                            console.log(`已匹配到手机号${item['1']}`)
+                        } else {
+                            console.log(`未匹配到手机号${item['1']}`);
+                        }
+                        done1(null)
+                    })
+                }, (err) => {
+                    done(err);
+                })
+            })
+    }
+], (err) => {
+    console.log("执行完成")
+})
+
